3fa70a14e80ea9d75db1209e3aa19f57a7431ec0
Nancy <chemutain@gmail.com>
2020-07-10 07:51:50 +0300

    more changes

---
+++ app/src/main/java/com/example/retrofit/RetrofitClient.java
@@ -0,0 +1,30 @@
+package com.example.retrofit;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String BASE_URL = "http://192.168.43.160/Retrofit/public/hello/";//localjost will not work, deploy the work to a live server
+    private static RetrofitClient mInstance;
+    private Retrofit retrofit;
+
+    private RetrofitClient(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance(){
+        if (mInstance == null){
+            mInstance = new RetrofitClient();
+        }
+        return mInstance;
+    }
+
+    public Api getApi(){
+        return retrofit.create(Api.class);
+    }
+}
